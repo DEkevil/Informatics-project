cc091ff279d18d60fbfb323cdf4dea2209af9473
DE <87647901+DEkevil@users.noreply.github.com>
2023-10-21 19:10:11 +0700

    lỗi tùm lum

--- Header.h
+++ Header.h
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-struct sanPham
+const int MAX = 100;
+
+//const int tenSanPham = 100;
+//const int maSanPham = 5;
+//const int gioiTinh = 3;
+//const int baoHanh = 2;
+
+struct sanPham //Túi sách
+{
+	string tenSanPham;
+	char maSanPham;
+	char gioiTinh;
+	char baoHanh; //tháng
+};
+
+struct Node //từng Node trong dslk lưu thông tin 1 sản phẩm
 {
 
-};
