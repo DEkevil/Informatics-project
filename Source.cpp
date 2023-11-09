86610267e4654d9b23458c980445ea99cb08b4ce
DE <87647901+DEkevil@users.noreply.github.com>
2023-11-09 21:50:32 +0700

    Fix full lỗi, lỗi mới ở hàm xếp giảm dần tăng dần giá tiền :))

--- Source.cpp
+++ Source.cpp
@@ -223,12 +223,134 @@ void xuat_DS_SP(DS_SanPham& ds)
 
 		for (int i = 1; i <= ds.soSanPham; i++)
 		{
-			cout << i + 1 << ". ";
+			cout << i << ". ";
 			xuat_SanPham(ds.ds_SP[i]);
 			cout << endl;
 		}
 	
 }
+
+void xoa_SanPham(DS_SanPham& ds)
+{  
+	int pos;
+	cout << "\nNhap vi tri san pham can xoa co tren danh sach: ";
+	cin >> pos;
+
+	while (pos < 1 || pos > ds.soSanPham)
+	{
+		cout << "So nhap nam ngoai danh sach, xin moi nhap lai:	";
+		cin >> pos;
+	}
+
+	for (int i = pos; i <= ds.soSanPham; i++)
+	{
+		ds.ds_SP[i] = ds.ds_SP[i + 1];
+	}
+	ds.soSanPham--;
+}
+void them_SanPham_Cuoi(DS_SanPham& ds)
+{
+	SanPham sp;
+	ds.soSanPham++;
+	nhap_SanPham(ds.ds_SP[ds.soSanPham]);
+}
+
+//void InterchangeSort(int a[], int n)
+//{
+//	for (int i = 0; i < n - 1; i++)
+//		for (int j = i + 1; j < n; j++)
+//			if (a[i] > a[j])
+//				swap(a[i], a[j]);
+//}
+void xep_giaTienSP_tangDan(DS_SanPham& ds) //InterchangeSort
+{
+	SanPham sp;
+
+	int n = ds.soSanPham;
+
+	for (int i = 1; i <= n - 1; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (ds.ds_SP[i].giaTienSP > ds.ds_SP[j].giaTienSP)
+				swap(ds.ds_SP[i], ds.ds_SP[j]);
+		}
+	}
+}
+void xep_giaTienSP_giamDan(DS_SanPham& ds) //InterchangeSort
+{
+	SanPham sp;
+	int n = ds.soSanPham;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i + 1; j <= n; j++)
+		{
+			if (ds.ds_SP[i].giaTienSP < ds.ds_SP[j].giaTienSP)
+				swap(ds.ds_SP[i], ds.ds_SP[j]);
+		}
+	}
+}
+
+void docFile(const char* tenfile, DS_SanPham& ds) 
+{
+	FILE* file;
+	errno_t file_in;
+	file_in = fopen_s(&file, tenfile, "r"); //chế độ đọc "r" của fopen_s
+	if (file_in != 0) {
+		cout << "Khong doc duoc file." << file_in << endl;
+		return;
+	}
+	int i = 1;
+	SanPham sp;
+
+	// Bỏ hàng đầu
+	//fscanf(file, "%*[^\n]\n");
+
+	//char maSP[30]; => %s : kiểu string or char
+	//char tenSP[30]; => % s : kiểu string or char
+	//char gioiTinhSP[5]; => % s : kiểu string or char
+	//int baoHanhSP; => %d: kiểu int
+	//float giaTienSP; => %f: kiểu float
+
+	while (fscanf(file, "%s %s %s %d %f\n", sp.maSP, sp.tenSP, sp.gioiTinhSP, &sp.baoHanhSP, &sp.giaTienSP) == 5) {
+		/*ds.ds_SP[i++] = sp;*/
+		ds.ds_SP[i] = sp;
+		i++;
+	}
+	ds.soSanPham = i-1;
+
+	/*
+	* Giải thích:
+	* File đang có 10 hàng (10 SP)
+	* Vì các vòng lặp start từ 1 => khai báo biến i = 1;
+	* Điều kiện trong vòng lặp while sẽ quét các giá trị trong hàng và gán vào các biến sp.
+	* Đủ 5 biến => tiếp tục chạy
+	* ds.soSanPham = i-1; Trừ 1 vì khi thực hiện xong 10 hàng thì biến i có giá trị là 11 và vòng lặp while sẽ dừng khi xét tiếp hàng tiếp theo (hàng 11) (đã debug kiểm tra) 
+	*/
+
+	fclose(file);
+}
+void xuatFile(const char* tenfile, const DS_SanPham& ds)
+{
+	FILE* file = nullptr;
+	errno_t file_in;
+
+	file_in = fopen_s(&file, tenfile, "w");
+	if (file_in != 0) {
+		cout << "Khong the ghi file." << endl;
+		return;
+	}
+
+	/*fprintf(file, "Ten san pham      Ma san pham           Gioi tinh            Bao hanh            Gia tien" << endl;);*/
+	fprintf(file, "MaSP\tTenSP\tGTSP\tBH\t\tGiaTien\n");
+	for (int i = 0; i < ds.soSanPham; ++i)
+	{
+		fprintf(file, "%s \t%s \t%s \t\t%d \t\t%.2f\n", ds.ds_SP[i].maSP, ds.ds_SP[i].tenSP, ds.ds_SP[i].gioiTinhSP, ds.ds_SP[i].baoHanhSP, ds.ds_SP[i].giaTienSP);
+	}
+	fclose(file);
+}
+
+
 void menu1(DS_SanPham ds)// menu gốc 
 {
 	nhap_DS_SP(ds);
@@ -263,117 +385,4 @@ void menu1(DS_SanPham ds)// menu gốc
 		xuat_DS_SP(ds);
 		xep_giaTienSP_tangDan(ds);
 	}
-}
-void xoa_SanPham(DS_SanPham& ds)
-{  
-	int pos;
-	cout << "\nNhap vi tri san pham can xoa co tren danh sach: ";
-	cin >> pos;
-
-	while (pos < 1 || pos > ds.soSanPham)
-	{
-		cout << "So nhap nam ngoai danh sach, xin moi nhap lai:	";
-		cin >> pos;
-	}
-
-	for (int i = pos; i <= ds.soSanPham; i++)
-	{
-		ds.ds_SP[i] = ds.ds_SP[i + 1];
-	}
-	ds.soSanPham--;
-}
-void them_SanPham_Cuoi(DS_SanPham& ds)
-{
-	SanPham sp;
-	ds.soSanPham++;
-	nhap_SanPham(ds.ds_SP[ds.soSanPham]);
-}
-//void InterchangeSort(int a[], int n)
-//{
-//	for (int i = 0; i < n - 1; i++)
-//		for (int j = i + 1; j < n; j++)
-//			if (a[i] > a[j])
-//				swap(a[i], a[j]);
-//}
-
-void xep_giaTienSP_tangDan(DS_SanPham& ds) //InterchangeSort
-{
-	SanPham sp;
-
-	int n = ds.soSanPham;
-
-	for (int i = 1; i <= n - 1; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (ds.ds_SP[i].giaTienSP > ds.ds_SP[j].giaTienSP)
-				swap(ds.ds_SP[i], ds.ds_SP[j]);
-		}
-	}
-}
-void xep_giaTienSP_giamDan(DS_SanPham& ds) //InterchangeSort
-{
-	SanPham sp;
-	int n = ds.soSanPham;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = i + 1; j <= n; j++)
-		{
-			if (ds.ds_SP[i].giaTienSP < ds.ds_SP[j].giaTienSP)
-				swap(ds.ds_SP[i], ds.ds_SP[j]);
-		}
-	}
-}
-void docFile(const char* tenfile, DS_SanPham& ds) 
-{
-	FILE* file;
-	errno_t file_in;
-	file_in = fopen_s(&file, tenfile, "r");
-	if (file_in != 0) {
-		cout << "Khong doc duoc file." << file_in << endl;
-		return;
-	}
-	int i = 0;
-	SanPham sp;
-
-	// Bỏ hàng đầu
-	//fscanf(file, "%*[^\n]\n");
-
-	//char maSP[30]; => %s : kiểu string or char
-	//char tenSP[30]; => % s : kiểu string or char
-	//char gioiTinhSP[5]; => % s : kiểu string or char
-	//int baoHanhSP; => %d: kiểu int
-	//float giaTienSP; => %f: kiểu float
-
-	while (fscanf(file, "%s %s %s %d %f\n", sp.maSP, sp.tenSP, sp.gioiTinhSP, &sp.baoHanhSP, &sp.giaTienSP) == 5) {
-		ds.ds_SP[i++] = sp;
-	}
-	ds.soSanPham = i;
-
-	fclose(file);
-}
-void xuatFile(const char* tenfile, const DS_SanPham& ds)
-{
-	FILE* file = nullptr;
-	errno_t file_in;
-
-	file_in = fopen_s(&file, tenfile, "w");
-	if (file_in != 0) {
-		cout << "Khong the ghi file." << endl;
-		return;
-	}
-
-	/*fprintf(file, "Ten san pham      Ma san pham           Gioi tinh            Bao hanh            Gia tien" << endl;);*/
-	fprintf(file, "MaSP\tTenSP\tGTSP\tBH\t\tGiaTien\n");
-	for (int i = 0; i < ds.soSanPham; ++i)
-	{
-		fprintf(file, "%s \t%s \t%s \t\t%d \t\t%.2f\n", ds.ds_SP[i].maSP, ds.ds_SP[i].tenSP, ds.ds_SP[i].gioiTinhSP, ds.ds_SP[i].baoHanhSP, ds.ds_SP[i].giaTienSP);
-	}
-	fclose(file);
-}
-
-
-void HamTest()
-{
-
 }
